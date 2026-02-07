fd266de8cc56d75d7fc3a2398db6927df75d4906
Pranshul13 <pranshulsharmaalg@gmail.com>
2026-02-07 22:23:09 +0530

    progress

---
+++ timeout.js
@@ -0,0 +1,30 @@
+function longTask() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Long task completed");
+    }, 10000); // task needs 10 sec
+  });
+}
+
+function timeout(ms) {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error("❌ Operation timed out"));
+    }, ms);
+  });
+}
+
+async function runTask() {
+  try {
+    const result = await Promise.race([
+      longTask(),
+      timeout(5000) // allow only 5 sec
+    ]);
+
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+runTask();
