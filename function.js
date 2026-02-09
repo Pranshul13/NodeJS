2143212296607987cc2d5d81494d9f30b5d99344
Pranshul13 <pranshul.sharma_cs23@gmail.com>
2026-02-09 23:02:45 +0530

    practices

--- function.js
+++ function.js
@@ -1,25 +1,32 @@
-function calculate(a, b, operation) {
-  switch (operation) {
-    case "add":
-      return a + b;
+// function calculate(a, b, operation) {
+//   switch (operation) {
+//     case "add":
+//       return a + b;
 
-    case "sub":
-      return a - b;
+//     case "sub":
+//       return a - b;
 
-    case "mul":
-      return a * b;
+//     case "mul":
+//       return a * b;
 
-    case "div":
-      if (b === 0) {
-        return "Error: Division by zero";
-      }
-      return a / b;
+//     case "div":
+//       if (b === 0) {
+//         return "Error: Division by zero";
+//       }
+//       return a / b;
 
-    default:
-      return "Invalid operation";
-  }
+//     default:
+//       return "Invalid operation";
+//   }
+// }
+// console.log(calculate(10, 5, "add")); // 15
+// console.log(calculate(10, 5, "sub")); // 5
+// console.log(calculate(10, 5, "mul")); // 50
+// console.log(calculate(10, 5, "div")); // 2
+
+
+function greet(name) {
+  console.log(`Hello ${name}`);
 }
-console.log(calculate(10, 5, "add")); // 15
-console.log(calculate(10, 5, "sub")); // 5
-console.log(calculate(10, 5, "mul")); // 50
-console.log(calculate(10, 5, "div")); // 2
+
+setTimeout(greet, 1500, "Pranshul");
