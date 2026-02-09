d4ad649ed3f2f768656e775c19e3f30149e61582
Pranshul13 <pranshulsharmaalg@gmail.com>
2026-02-09 22:53:07 +0530

    Added calculator functions

---
+++ function.js
@@ -0,0 +1,25 @@
+function calculate(a, b, operation) {
+  switch (operation) {
+    case "add":
+      return a + b;
+
+    case "sub":
+      return a - b;
+
+    case "mul":
+      return a * b;
+
+    case "div":
+      if (b === 0) {
+        return "Error: Division by zero";
+      }
+      return a / b;
+
+    default:
+      return "Invalid operation";
+  }
+}
+console.log(calculate(10, 5, "add")); // 15
+console.log(calculate(10, 5, "sub")); // 5
+console.log(calculate(10, 5, "mul")); // 50
+console.log(calculate(10, 5, "div")); // 2
