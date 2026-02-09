cfc0a33891be25b590232cf52cd862ed31223115
Pranshul13 <pranshulsharmaalg@gmail.com>
2026-02-09 22:48:10 +0530

    progress

--- timeout.js
+++ timeout.js
@@ -1,31 +1,35 @@
-function longTask() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Long task completed");
-    }, 10000); // task needs 10 sec
-  });
-}
+// function longTask() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("Long task completed");
+//     }, 10000); // task needs 10 sec
+//   });
+// }
 
-function timeout(ms) {
-  return new Promise((_, reject) => {
-    setTimeout(() => {
-      reject(new Error("❌ Operation timed out"));
-    }, ms);
-  });
-}
+// function timeout(ms) {
+//   return new Promise((_, reject) => {
+//     setTimeout(() => {
+//       reject(new Error("❌ Operation timed out"));
+//     }, ms);
+//   });
+// }
 
-async function runTask() {
-  try {
-    const result = await Promise.race([
-      longTask(),
-      timeout(5000) // allow only 5 sec
-    ]);
+// async function runTask() {
+//   try {
+//     const result = await Promise.race([
+//       longTask(),
+//       timeout(5000) // allow only 5 sec
+//     ]);
 
-    console.log(result);
-  } catch (error) {
-    console.log(error.message);
-  }
-}
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
 
 runTask();
-//hello
+
+setTimeout(() => {
+  console.log("Runs after 2 seconds");
+}, 2000);
+
